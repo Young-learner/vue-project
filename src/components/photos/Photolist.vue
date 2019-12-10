6a6d0ef1a4ba7d7de712e38dcb43683eff9cef8b
Young-learner <name_tl@163.com>
2019-12-10 11:26:39 +0800

    第7次提交

--- src/components/photos/Photolist.vue
+++ src/components/photos/Photolist.vue
@@ -5,20 +5,8 @@
             <div id="sliderSegmentedControl"
                  class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
                 <div class="mui-scroll">
-                    <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-                        推荐
-                    </a>
-                    <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-                        热点
-                    </a>
-                    <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-                        北京
-                    </a>
-                    <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-                        社会
-                    </a>
-                    <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-                        娱乐
+                    <a :class="['mui-control-item', item.id== 0 ? 'mui-active':'']" v-for="item in cates" :key="item.id">
+                        {{item.title}}
                     </a>
                 </div>
             </div>
@@ -34,7 +22,12 @@
 
     export default {
         data(){
-            return {};
+            return {
+                cates:[],//所有分类的列表数据
+            };
+        },
+        created(){
+            this.getAllCategory();
         },
         mounted(){//当组件中的Dom结构被渲染并放到页面中,会执行这个钩子函数
             mui('.mui-scroll-wrapper').scroll({
@@ -42,6 +35,19 @@
             });
         },
         methods:{
+            getAllCategory(){
+                //获取所有图片分类
+                this.$http.get('api/getmgcategory').then(result=>{
+                    if(result.body.status === 0){
+                        //手动拼接完整列表
+                        result.body.message.unshift({
+                            title:'全部',
+                            id:0
+                        });
+                        this.cates = result.body.message;
+                    }
+                })
+            }
 
         }
     }
